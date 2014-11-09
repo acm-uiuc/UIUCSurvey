aef13eefb36425be30a571ee33a9fb248e431ab1
tciemniak <tciemniak@yahoo.com>
2014-11-08 21:30:42 -0600

    Updated distributed and random function
    Changed distributed to int from boolean.

--- backend/distribute_surveys.py
+++ backend/distribute_surveys.py
@@ -7,30 +7,28 @@ db = client.node_auth # Connect to node_auth db
 
 # Goal: Get the surveys that have yet to be distributed. Assign them to a proper sample based on the target.
 
-surveys_to_handle = db.surveys.find({"distributed" : False})
+surveys_to_handle = db.surveys.find({"distributed" : {"$gt" : 0}})
 
-def distribute_survey_all( survey, number_of_recipients ):
+def distribute_survey_all( survey ):
     print "Survey _id: %s" % survey['_id']
+    number_of_recipients = survey['distributed']
     users = db.users.find({"pendingSurvey": {"$exists" : False}, "surveys_taken" : {"$ne" : survey['_id']}})
     count = users.count()
     print "Found %s qualified users" % count
 
-    allocated = 0
+    recipients_left = number_of_recipients
+    for user in users:
+        if random.random() < recipients_left/count:
+            user.update({"pendingSurvey" : survey['_id']})
+            print "Updated user: %s" % user['email']
+            db.users.save(user)
+            recipients_left -= 1
+        count -= 1
 
-    while allocated < number_of_recipients and allocated < count: # While there are more to be allocated and we have enough users to allocate to
-        for user in users:
-            if random.random() < number_of_recipients/count:
-                user.update({"pendingSurvey" : survey['_id']})
-                print "Updated user: %s" % user['email']
-                db.users.save(user)
-                allocated = allocated + 1
-
-    # Check if done distributing
-    if allocated == number_of_recipients:
-        survey.update({"distributed" : 0})
-        db.surveys.save(survey)
+    survey.update({"distributed" : recipients_left})
+    db.surveys.save(survey)
 
 for survey in surveys_to_handle:
     if survey['target'] == False: # The surveys that can go to anyone
-        distribute_survey_all(survey, 50) # Hard coded!
+        distribute_survey_all(survey) # Hard coded!
 
